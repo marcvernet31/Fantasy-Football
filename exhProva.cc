8706e53ee5ec4d3823ddeb00f3879008979d3e8b
marcvernet31 <44305244+marcvernet31@users.noreply.github.com>
2019-11-15 12:28:53 +0100

    Add files via upload

--- exhProva.cc
+++ exhProva.cc
@@ -27,17 +27,18 @@ struct Equip{
 
 vector<Jugador> database;
 
-bool sortVal(const Jugador &A, const Jugador &B)
-{
-    return B.punts < A.punts;
-}
+/*
+bool sortVal(const Jugador &A, const Jugador &B) return A.punts < B.punts;
+*/
 
-//string outputFile;
-int parser(int argc, char** argv, int J, priority_queue < pair<int, string> >& valorJugador) {
+int parser(int argc, char** argv, int J, int& valorJugadorMax, int& preuJugadorMin) {
   if (argc != 4) {
     cout << "Syntax: " << argv[0] << " data_base.txt" << endl;
     exit(1);
   }
+
+  priority_queue<pair<int,string> > valorJugadors;
+
   ifstream in(argv[1]);
   while (not in.eof()) {
     string nom, posicio, club;
@@ -54,11 +55,12 @@ int parser(int argc, char** argv, int J, priority_queue < pair<int, string> >& v
     Jugador P{nom,posicio,preu,club,punts};
     if(P.preu <= J){
       database.push_back(P);
-      valorJugador.push({P.punts, P.nom});
+      if(P.punts > 0) valorJugadors.push({P.punts, P.nom});
+      if(preuJugadorMin > P.preu and P.preu != 0) preuJugadorMin = P.preu;
     }
   }
-  sort(database.begin(), database.end(), sortVal);
-
+  //sort(database.begin(), database.end(), sortVal);
+  valorJugadorMax = valorJugadors.top().first;
   in.close();
   return 0;
 }
@@ -94,10 +96,14 @@ void output(const vector<Jugador>& current, int punts, const string& outputFile)
   cout << "Preu: " << preu << endl;
 }
 
+/*
 bool checkEquip(Jugador J, int puntsActual, int puntsMax, Equip confEquip,
-priority_queue < pair<int, string> >& valorJugador){
+  priority_queue < pair<int, string> >& valorJugador){
   int n = 11 - confEquip.POR - confEquip.DEF - confEquip.MIG - confEquip.DAV;
-  int actualMax = valorJugador.top().first;
+
+  int actualMax;
+  if(valorJugador.empty()) actualMax = 0;
+  else actualMax = valorJugador.top().first;
 
   if(J.nom == valorJugador.top().second)valorJugador.pop();
 
@@ -105,14 +111,30 @@ priority_queue < pair<int, string> >& valorJugador){
 
   return true;
 }
+*/
+
+bool checkPunts(const Equip& confEquip, const int& valorJugadorMax, const int& puntsActual, const int& puntsMax){
+  if(((11 - confEquip.POR - confEquip.DEF - confEquip.MIG - confEquip.DAV)*valorJugadorMax+puntsActual) >= puntsMax){
+    return true;
+  }
+  return false;
+}
+
+bool checkPreu(const Equip& confEquip, const Equip& restriccions, const int& preuJugadorMin){
+  if(((11 - confEquip.POR - confEquip.DEF - confEquip.MIG - confEquip.DAV)*preuJugadorMin + confEquip.pressupost) < restriccions.pressupost){
+    return true;
+  }
+  return false;
+}
 
 //Cerca exhaustiva i poda
-void exhaustive(int i, vector<Jugador>& current,
-  vector<bool>& usat, const Equip& restriccions, Equip confEquip, int puntsActual,
-  int& puntsMax, const string& outputFile, priority_queue < pair<int, string> > valorJugador){
-    if(i == 11){
+void exhaustive(int i, vector<Jugador>& iteracioActual, vector<bool>& usat,
+    const Equip& restriccions, Equip confEquip, int puntsActual,
+    int& puntsMax, const string& outputFile, const int& valorJugadorMax, const int& preuJugadorMin){
 
-      for(Jugador J : current) cout << J.nom << " ";
+    if(i == 11){
+      ////////////////////////////////////////////////
+      for(Jugador J : iteracioActual) cout << J.nom << " ";
       cout << endl;
       /*
       if(puntsActual > puntsMax){
@@ -120,53 +142,53 @@ void exhaustive(int i, vector<Jugador>& current,
         output(current, puntsActual, outputFile);
       }
       */
+      ///////////////////////////////////////////////
     }
     else{
-      for(unsigned int j = 0; j < database.size(); ++j){
+      int n = database.size();
+      for(unsigned int j = 0; j < n; ++j){
         if(not usat[j]){
-          current[i] = database[j];
+          iteracioActual[i] = database[j];
           usat[j] = true;
-          puntsActual += current[i].punts;
+          puntsActual += iteracioActual[i].punts;
+          confEquip.pressupost += iteracioActual[i].preu;
+          int num = 11;
+          if(  checkPunts(confEquip, valorJugadorMax, puntsActual, puntsMax)
+                and checkPreu(confEquip, restriccions, preuJugadorMin)
+               and confEquip.pressupost <= restriccions.pressupost){
 
-          confEquip.pressupost += current[i].preu;
-          if( (checkEquip(current[i], puntsActual, puntsMax, confEquip, valorJugador) and confEquip.pressupost <= restriccions.pressupost)){
-
-            if(current[i].posicio == "por"){
+            if(iteracioActual[i].posicio == "por"){
               ++confEquip.POR;
               if(confEquip.POR <= restriccions.POR){
-                exhaustive(i+1, current, usat, restriccions, confEquip, puntsActual, puntsMax, outputFile, valorJugador);
+                exhaustive(i+1, iteracioActual, usat, restriccions, confEquip, puntsActual, puntsMax, outputFile, valorJugadorMax, preuJugadorMin);
               }
               --confEquip.POR;
             }
-
-            else if(current[i].posicio == "dav"){
+            else if(iteracioActual[i].posicio == "dav"){
               ++confEquip.DAV;
               if(confEquip.DAV <= restriccions.DAV){
-                exhaustive(i+1, current, usat, restriccions, confEquip, puntsActual, puntsMax, outputFile, valorJugador);
+                exhaustive(i+1, iteracioActual, usat, restriccions, confEquip, puntsActual, puntsMax, outputFile, valorJugadorMax, preuJugadorMin);
               }
               --confEquip.DAV;
             }
-
-            else if(current[i].posicio == "mig"){
+            else if(iteracioActual[i].posicio == "mig"){
               ++confEquip.MIG;
-              confEquip.pressupost += current[i].preu;
+              confEquip.pressupost += iteracioActual[i].preu;
               if(confEquip.MIG <= restriccions.MIG){
-                exhaustive(i+1, current, usat, restriccions, confEquip, puntsActual, puntsMax, outputFile, valorJugador);
+                exhaustive(i+1, iteracioActual, usat, restriccions, confEquip, puntsActual, puntsMax, outputFile, valorJugadorMax, preuJugadorMin);
               }
               --confEquip.MIG;
             }
-
-            else if(current[i].posicio == "def"){
+            else if(iteracioActual[i].posicio == "def"){
               ++confEquip.DEF;
               if(confEquip.DEF <= restriccions.DEF){
-                exhaustive(i+1, current, usat, restriccions, confEquip, puntsActual, puntsMax, outputFile, valorJugador);
+                exhaustive(i+1, iteracioActual, usat, restriccions, confEquip, puntsActual, puntsMax, outputFile, valorJugadorMax, preuJugadorMin);
               }
               --confEquip.DEF;
             }
           }
-          puntsActual -= current[i].punts;
-
-          confEquip.pressupost -= current[i].preu;
+          puntsActual -= iteracioActual[i].punts;
+          confEquip.pressupost -= iteracioActual[i].preu;
           usat[j] = false;
         }
       }
@@ -183,28 +205,21 @@ void input(int argc, char** argv, int& N1, int& N2, int& N3, int& T, int& J) {
 
 
 int main(int argc, char** argv){
+  string outputFile = argv[3];
   int N1, N2, N3, T, J;
   input(argc, argv, N1, N2, N3, T, J);
 
-
-  string outputFile = argv[3];
-  priority_queue < pair<int, string> > valorJugador;
-  parser(argc, argv, J, valorJugador); //inserir jugadors a database i filtrar per J(pressupost jugador)
-
-/*
-  while(not valorJugador.empty()){
-    cout << valorJugador.top().first << " " << valorJugador.top().second << endl;
-    valorJugador.pop();
-  }
-*/
-
-  vector<Jugador> current(11);  //string actual a la recursivitat
+  int valorJugadorMax;
+  int preuJugadorMin = 500000000;
+  parser(argc, argv, J, valorJugadorMax, preuJugadorMin); //inserir jugadors a database i filtrar per J(pressupost jugador)
+  cout << preuJugadorMin << endl;
+  vector<Jugador> iteracioActual(11);  //string actual a la recursivitat
   vector<bool> usat(database.size(), false); //vector de bool per no repetir jugadors
   Equip restriccions {1, N1, N2, N3, T}; //Per comprovar que la configuracio de l'equip sigui correcte
   Equip confEquip {0,0,0,0,0};
-  int puntsMax = 0;
+  int puntsMax = 0; //puntacio maxim obtinguda per un equip
 
   //startTime = std::clock();
-  exhaustive(0, current, usat, restriccions, confEquip, 0, puntsMax, outputFile, valorJugador); //crea totes les combinacions possibles
+  exhaustive(0, iteracioActual, usat, restriccions, confEquip, 0, puntsMax, outputFile, valorJugadorMax, preuJugadorMin);
 
 }
